9e0002f89afd770f303c550756371aba87390fab
Will Jackson <willjackson00@gmail.com>
2025-11-12 02:21:56 -0500

    certbot updates

--- pkg/ssl/certbot.go
+++ pkg/ssl/certbot.go
@@ -188,16 +188,6 @@ func EnhanceSSLConfig(domain string) error {
 	// Add enhanced SSL configuration after the ssl_certificate_key line
 	enhancedConfig := `
     # Enhanced SSL Security Settings
-    ssl_protocols TLSv1.2 TLSv1.3;
-    ssl_ciphers 'ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384';
-    ssl_prefer_server_ciphers off;
-    
-    # SSL session configuration
-    ssl_session_timeout 1d;
-    ssl_session_cache shared:SSL:50m;
-    ssl_session_tickets off;
-    
-    # OCSP stapling
     ssl_stapling on;
     ssl_stapling_verify on;
     resolver 8.8.8.8 8.8.4.4 valid=300s;
